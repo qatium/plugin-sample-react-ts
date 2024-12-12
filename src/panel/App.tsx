ad92ace61a041856d7401537c9616a31fb69956d
Alex Torres <alex.torres@qatium.com>
2024-12-12 10:37:20 +0100

    Use styles from SDK

--- src/panel/App.tsx
+++ src/panel/App.tsx
@@ -15,15 +15,19 @@ function App() {
 
   const content = selectedElement ?
     (
-      <button onClick={() => closeValve(selectedElement.id)}>
-        Close selected valve
-      </button>
+      <div>
+        <p>
+          <b>Valve selected</b>: {selectedElement.id}
+        </p>
+        <button onClick={() => closeValve(selectedElement.id)}>
+          Close selected valve
+        </button>
+      </div>
     )
     : <p>First select a valve</p>
 
   return (
     <>
-      <h1>Valve closer</h1>
       {content}
     </>
   )
--- src/plugin/plugin.ts
+++ src/plugin/plugin.ts
@@ -1,19 +1,12 @@
 import { Plugin } from "@qatium/sdk/plugin";
-import { SDK } from "@qatium/sdk";
+import { ElementIdentifier, SDK } from "@qatium/sdk";
 import { MessageToEngine, MessageToUI } from '../communication/messages';
 
 export class MyPlugin implements Plugin {
   selectedElement: ReturnType<SDK["map"]["getSelectedElement"]>;
 
-  run() {
-    const newSelectedElement = sdk.map.getSelectedElement()
-
-    if (newSelectedElement?.id === this.selectedElement?.id) {
-      return;
-    }
-
-    this.selectedElement = newSelectedElement ? { ...newSelectedElement } : undefined;
-
+  onElementSelected(element?: ElementIdentifier) {
+    this.selectedElement = element ? { ...element } : undefined;
     return sdk.ui.sendMessage<MessageToUI>({
       event: "selected-element",
       selectedElement: this.selectedElement
